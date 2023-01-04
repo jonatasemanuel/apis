be5144e87094c08f0a41acbe11408d797d6e2db8
Jonatas Silva [WSL2] <jonatassilvaemanuel@gmail.com>
2023-01-03 23:33:16 -0300

    first commit

---
+++ python_repos.py
@@ -0,0 +1,12 @@
+import requests
+
+
+# Faz uma chamada de API e armazena a resposta
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print(f'Status code: {r.status_code}')
+
+# Armazena a resposta da API em uma variável
+response_dict = r.jason()
+# Processa o resultado
+print(response_dict.keys())
