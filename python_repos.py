7c6d90e303e61662432be5bbc85c539cf326fb44
Jonatas Silva [WSL2] <jonatassilvaemanuel@gmail.com>
2023-01-04 03:14:16 -0300

    2 commit

--- python_repos.py
+++ python_repos.py
@@ -7,6 +7,26 @@ r = requests.get(url)
 print(f'Status code: {r.status_code}')
 
 # Armazena a resposta da API em uma variável
-response_dict = r.jason()
-# Processa o resultado
-print(response_dict.keys())
+response_dict = r.json()
+print(f"Total repositories: {response_dict['total_count']}")
+# Explora informações sobre os repositórios
+repo_dicts = response_dict['items']
+print(f'Repositories returned: {len(repo_dicts)}')
+
+# Analisa o primeiro repositório:
+# repo_dict = repo_dicts[0]
+# print(f'\nKeys: {len(repo_dict)}')
+# for key in sorted(repo_dict.keys()):
+#    print(key)
+
+# Mostra todos os repositórios possiveis na tela:
+for repo_dict in repo_dicts:
+    print('\nSelected information about first repository: ')
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
+
